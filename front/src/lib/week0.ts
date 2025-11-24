c1b4c7ce0583b40d35035836155bd46dfe89fcdc
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2025-11-23 17:00:22 -0800

    add lift to plan

--- front/src/lib/week0.ts
+++ front/src/lib/week0.ts
@@ -1,4 +1,4 @@
-import { EasyRun, Exercise, WeekPlan } from "./data";
+import { EasyRun, Exercise, WeekPlan, TempoRun } from "./data";
 import { IntervalsRun, Rep, Rest, Set } from "./intervals";
 import { quickLiftSets } from "./util";
 
@@ -32,34 +32,111 @@ export const week0: WeekPlan = new WeekPlan([
     {
         date: new Date("2025-11-25"),
         run: new IntervalsRun(
-            1.5,
+            1.7,
             [
                 new Set([
+                    new Rep(200, 34),
+                    new Rest(undefined, 200),
                     new Rep(200, 32),
                     new Rest(undefined, 200),
-                    new Rep(200, 30)
+                    new Rep(200, 31),
                 ])
             ],
-            1
+            1.7
         )
     },
     {
         date: new Date("2025-11-26"),
         run: new EasyRun(3),
+        lift: {
+            exercises: new Map(
+                [
+                    [
+                        Exercise.DumbbellInclineBench,
+                        quickLiftSets(8, [50, 70, 80])
+                    ],
+                    [
+                        Exercise.BicepCurl,
+                        quickLiftSets(8, [30, 40, 50])
+                    ],
+                    [
+                        Exercise.OverheadPress,
+                        quickLiftSets(8, [50, 60])
+                    ],
+                    [
+                        Exercise.Pullup,
+                        quickLiftSets(6, [0, 0, 0])
+                    ],
+                    [
+                        Exercise.DumbbellRow,
+                        quickLiftSets(12, [35, 40])
+                    ],
+                ]
+            )
+        }
     },
     {
         date: new Date("2025-11-27"),
-        run: new EasyRun(5),
+        run: new EasyRun(3.5),
+        lift: {
+            exercises: new Map(
+                [
+                    [
+                        Exercise.BulgarianSplitSquat,
+                        quickLiftSets(8, [40, 60, 70])
+                    ],
+                    [
+                        Exercise.Deadlift,
+                        quickLiftSets(8, [95, 115])
+                    ],
+                    [
+                        Exercise.DumbbellRow,
+                        quickLiftSets(12, [35, 40])
+                    ],
+                    [
+                        Exercise.Pullup,
+                        quickLiftSets(6, [40, 50])
+                    ]
+                ]
+            )
+        }
     },
     {
         date: new Date("2025-11-28"),
-        run: new EasyRun(5),
+        run: new TempoRun(1, 2, 1),
     },
     {
         date: new Date("2025-11-29"),
+        run: new EasyRun(3),
+        lift: {
+            exercises: new Map(
+                [
+                    [
+                        Exercise.DumbbellInclineBench,
+                        quickLiftSets(8, [50, 70, 70])
+                    ],
+                    [
+                        Exercise.BicepCurl,
+                        quickLiftSets(8, [30, 40, 40])
+                    ],
+                    [
+                        Exercise.OverheadPress,
+                        quickLiftSets(8, [50, 50])
+                    ],
+                    [
+                        Exercise.Pullup,
+                        quickLiftSets(6, [0, 0])
+                    ],
+                    [
+                        Exercise.DumbbellRow,
+                        quickLiftSets(12, [35, 35])
+                    ],
+                ]
+            )
+        }
     },
     {
         date: new Date("2025-11-30"),
-        run: new EasyRun(5),
+        run: new EasyRun(3.5),
     }
 ]);
