d7e8903f11bd7b844ebf2aa51b6d7cc37dff5896
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2025-11-23 16:15:04 -0800

    init

---
+++ front/src/lib/data.ts
@@ -0,0 +1,130 @@
+enum RunKind {
+    EASY = 'easy',
+    TEMPO = 'tempo',
+}
+
+interface Run {
+    kind: RunKind;
+    totalMiles(): number;
+}
+
+export class EasyRun implements Run {
+    kind: RunKind;
+    miles: number;
+
+    constructor(miles: number) {
+        this.kind = RunKind.EASY;
+        this.miles = miles;
+    }
+
+    totalMiles(): number {
+        return this.miles;
+    }
+}
+
+export class TempoRun implements Run {
+    kind: RunKind
+    warmupMiles: number;
+    tempoMiles: number;
+    cooldownMiles: number;
+
+    constructor(warmupMiles: number, tempoMiles: number, cooldownMiles: number) {
+        this.kind = RunKind.TEMPO;
+        this.warmupMiles = warmupMiles;
+        this.tempoMiles = tempoMiles;
+        this.cooldownMiles = cooldownMiles;
+    }
+
+    totalMiles(): number {
+        return this.warmupMiles + this.tempoMiles + this.cooldownMiles;
+    }
+}
+
+export enum Exercise {
+    AnteriorBunkie = "Anterior Bunkie",
+    BarbellRdl = "Barbell Romanian Deadlift",
+    Bench = "Bench",
+    BicepCurl = "Bicep Curl",
+    BulgarianSplitSquat = "Bulgarian Split Squat",
+    CableSeatedRow = "Cable Seated Row",
+    CoreBicycle = "Core Bicycle",
+    CoreRow = "Core Row",
+    Crunch = "Crunch",
+    DeadBugs = "Dead Bugs",
+    DeadHang = "Dead Hang",
+    Deadlift = "Deadlift",
+    DeclineSitUp = "Decline Situp",
+    Dip = "Dip",
+    DumbbellBench = "Dumbbell Bench",
+    DumbbellFly = "Dumbbell Fly",
+    DumbbellInclineBench = "Dumbbell Incline Bench",
+    DumbbellOneLegOneArmRomanialDeadLift = "Dumbbell One Leg One Arm Romanian Deadlift",
+    DumbbellRdl = "Dumbbell Romanian Deadlift",
+    DumbbellRow = "Dumbbell Row",
+    DumbbellUprightRow = "Dumbbell Upright Row",
+    FacePull = "Face Pull",
+    FarmerCary = "Farmer Cary",
+    ForwardDownBunkie = "Forward Down Bunkie",
+    FourtyFiveDegreeBackExtension = "45 Degree Back Extension",
+    GluteBridge = "Glute Bridge",
+    HammerCurl = "Hammer Curl",
+    HamstringBridge = "Hamstring Bridge",
+    HamstringCurl = "Hamstring Curl",
+    HollowBodyHold = "Hollow Body Hold",
+    HollowHold = "Hollow Hold",
+    InnerDownBunkie = "Inner Down Bunkie",
+    LatPulldown = "Lat Pulldown",
+    LateralBunkie = "Lateral Bunkie",
+    LateralRaise = "Lateral Raise",
+    LedgeTricepDip = "Ledge Tricep Dip",
+    LegRaise = "Leg Raise",
+    LemonSqueezers = "Lemon Squeezers",
+    MedBallHipToss = "Med Ball Hip Toss",
+    MedBallSeatedRussianTwist = "Med Ball Seated Russian Twist",
+    MedBallSpeedSkaterJumps = "Med Ball Speed Skater Jumps",
+    MedialBunkie = "Medial Bunkie",
+    MountainClimber = "Mountain Climber",
+    NinetyDegreeBackExtension = "90 Degree Back Extension",
+    NordicCurl = "Nordic Curl",
+    OneLegBuck = "One Leg Buck",
+    OuterDownBunkie = "Outer Down Bunkie",
+    OverheadPress = "Overhead Press",
+    OxidativeSquat = "Oxidative Squat",
+    Plank = "Plank",
+    PlateMcGillCrunch = "Plate McGill Crunch",
+    PreacherCurl = "Preacher Curl",
+    Pullup = "Pullup",
+    Pushup = "Pushup",
+    ReverseCrunch = "Reverse Crunch",
+    RomanianDeadLift = "Romanian Deadlift",
+    Row = "Row",
+    RussianTwist = "Russian Twist",
+    ShortFootExercise = "Short Foot Exercise",
+    SideCrunch = "Side Crunch",
+    SideLegLift = "Side Leg Lift",
+    SingleLegCalfRaise = "Single Leg Calf Raise",
+    SingleLegGluteBridge = "Single Leg Glute Bridge",
+    SingleLegHamstringBridge = "Single Leg Hamstring Bridge",
+    SingleLegSquat = "Single Leg Squat",
+    SingleLegStairCalfRaise = "Single Leg Stair Calf Raise",
+    SingleLegTibRaise = "Single Leg Tib Raise",
+    Situp = "Situp",
+    SmithSquat = "Smith Machine Squat",
+    SpeedSkaterJumps = "Speed Skater Jumps",
+    Squat = "Squat",
+    SupineKneeDrive = "Supine Knee Drive",
+    TibBar = "Tib Bar",
+    TibialisRaise = "Tibialis Raise",
+    TricepExtension = "Tricep Extension",
+    UnderhandLatPulldown = "Underhand Lat Pulldown",
+    UprightRow = "Upright Row",
+}
+
+export interface LiftPlan {
+    exercises: Map<Exercise, { sets: number; reps: number }>;
+}
+export interface DayPlan {
+    lift?: LiftPlan;
+    run?: Run;
+    date: Date;
+}
---
+++ front/src/lib/week0.ts
@@ -0,0 +1,31 @@
+import { EasyRun, type DayPlan } from "./data";
+
+export const week0: DayPlan[] = [
+    {
+        date: new Date("2025-11-24"),
+        run: new EasyRun(3),
+    },
+    {
+        date: new Date("2025-11-25"),
+        run: new EasyRun(4),
+    },
+    {
+        date: new Date("2025-11-26"),
+        run: new EasyRun(3),
+    },
+    {
+        date: new Date("2025-11-27"),
+        run: new EasyRun(5),
+    },
+    {
+        date: new Date("2025-11-27"),
+        run: new EasyRun(5),
+    },
+    {
+        date: new Date("2025-11-27"),
+    },
+    {
+        date: new Date("2025-11-27"),
+        run: new EasyRun(5),
+    }
+];
